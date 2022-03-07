775a65e7292ccc5f52c0af8f392a80be12004e5d
AlexanderSamisko <83254509+AlexanderSamisko@users.noreply.github.com>
2022-03-07 16:46:16 +0300

    v1.0

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,7 +12,16 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
-
+    try {
+      this.element = element;
+      if (element.innerText == ``) {
+        throw new Error("Элемент пуст!");
+      }
+    } catch(e) {
+      console.error(e.message);
+    }
+    
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +30,11 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    let buttons = this.element.querySelectorAll(`[data-dismiss="modal"]`);
+    let buttonsArr = [...buttons];
+    for ( let i = 0; i < buttonsArr.length; i++) {
+    buttonsArr[i].addEventListener(`click`, this.onClose.bind(this));  
+    }
   }
 
   /**
@@ -29,19 +42,20 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
+    this.close();
+    e.preventDefault();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = "block";
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display = "none";
   }
 }
